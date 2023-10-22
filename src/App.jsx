4a87aa5828413ac7f9e5da95016085a07bee27b9
Jafar Rezaei <sayjeyhi@gmail.com>
2023-10-23 00:33:15 +0200

    improve scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ import { framerMotionConfig } from './config'
 import state from './state.json'
 
 // TODO: Remove this in production
-import studio from '@theatre/studio'
-import extension from '@theatre/r3f/dist/extension'
-if (import.meta.env.DEV) {
-  studio.initialize()
-  studio.extend(extension)
-}
+// import studio from '@theatre/studio'
+// import extension from '@theatre/r3f/dist/extension'
+// if (import.meta.env.DEV) {
+//   studio.initialize()
+//   studio.extend(extension)
+// }
 
 function App() {
   const [section, setSection] = useState(0)
--- src/components/ScrollManager.jsx
+++ src/components/ScrollManager.jsx
@@ -1,49 +1,52 @@
-import { useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
+import { useScroll } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { gsap } from 'gsap'
+import { useEffect, useRef } from 'react'
 
-export const ScrollManager = (props) => {
-  const { section, onSectionChange } = props;
+export const ScrollManager = props => {
+  const { section, onSectionChange } = props
 
-  const data = useScroll();
-  const lastScroll = useRef(0);
-  const isAnimating = useRef(false);
+  const data = useScroll()
+  const lastScroll = useRef(0)
+  const isAnimating = useRef(false)
 
-  data.fill.classList.add("top-0");
-  data.fill.classList.add("absolute");
+  data.fill.classList.add('top-0')
+  data.fill.classList.add('absolute')
 
   useEffect(() => {
     gsap.to(data.el, {
-      duration: 1,
+      duration: 0.5 + (section || 1),
       scrollTop: section * data.el.clientHeight,
       onStart: () => {
-        isAnimating.current = true;
+        isAnimating.current = true
       },
       onComplete: () => {
-        isAnimating.current = false;
-      },
-    });
-  }, [section]);
+        isAnimating.current = false
+      }
+    })
+  }, [section])
 
   useFrame(() => {
     if (isAnimating.current) {
-      lastScroll.current = data.scroll.current;
-      return;
+      lastScroll.current = data.scroll.current
+      return
     }
 
-    const curSection = Math.floor(data.scroll.current * data.pages);
+    const curSection = Math.floor(data.scroll.current * data.pages)
     if (data.scroll.current > lastScroll.current && curSection === 0) {
-      onSectionChange(1);
+      onSectionChange(1)
     }
-    if (
-      data.scroll.current < lastScroll.current &&
-      data.scroll.current < 1 / (data.pages - 1)
-    ) {
-      onSectionChange(0);
+    if (data.scroll.current - 0.002 > lastScroll.current && curSection === 1) {
+      onSectionChange(2)
     }
-    lastScroll.current = data.scroll.current;
-  });
+    if (data.scroll.current - 0.002 > lastScroll.current && curSection === 2) {
+      onSectionChange(3)
+    }
+    if (data.scroll.current < lastScroll.current && data.scroll.current < 1 / (data.pages - 1)) {
+      onSectionChange(0)
+    }
+    lastScroll.current = data.scroll.current
+  })
 
-  return null;
-};
+  return null
+}
