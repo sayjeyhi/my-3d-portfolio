17b6b79c77a894cea5cdac9736c647e2ad82eca8
Jafar Rezaei <sayjeyhi@gmail.com>
2023-12-17 11:41:02 +0100

    fix: improve loading animation

--- src/loading.jsx
+++ src/loading.jsx
@@ -1,7 +1,11 @@
 export function Loading() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <span className="loader" />
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="loader">
+        <div className="box"></div>
+        <div className="shadow"></div>
+      </div>
+      <p style={{ fontFamily: "'Inter', tahoma, arial, serif" }}>Loading...</p>
     </div>
   )
 }
